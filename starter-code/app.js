217cceb2237872ef2825c7c273c9d10959f86929
Vitaly Drobyshev <vitaliy.drobyshev@gmail.com>
2019-09-13 14:03:25 -0700

    Solves the second problem

--- starter-code/app.js
+++ starter-code/app.js
@@ -11,7 +11,7 @@ Test this function by hand in the console to get it working, and when you think 
 function sum(a, b) { //eslint-disable-line
   var numSum = a + b;
   var sumString = `The sum of ${a} and ${b} is ${numSum}.`;
-  var array = [numSum,sumString];
+  var array = [numSum, sumString];
   return array;
 
 }
@@ -31,11 +31,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
+  var firstElement = a * b;
+  var secondElement = `The product of ${a} and ${b} is ${firstElement}.`;
+  var array = [firstElement, secondElement];
+  return array;
 
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
