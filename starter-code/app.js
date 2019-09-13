27bee5f744aaa78d877bfd4cb6d9567e1807ecfc
Vitaly Drobyshev <vitaliy.drobyshev@gmail.com>
2019-09-13 15:25:59 -0700

    Solves the fourth problem

--- starter-code/app.js
+++ starter-code/app.js
@@ -67,7 +67,7 @@ function sumAndMultiply(a, b, c) { //eslint-disable-line
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-testSumAndMultiply(4,7,5);
+testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -85,12 +85,17 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  var sumIntro = sum(sumArr[0], sumArr[1])[0];
+  var sumFinal = sum(sumIntro, sumArr[2])[0];
+  var string = `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sumFinal} is their sum.`;
+  var finalString = [sumFinal,string];
+  console.log ('2,3,4 was passed in as an array of numbers, and 9 is their sum.');
+  return finalString;
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
